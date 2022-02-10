0e72ad6d1db86ef057eeeeb2a2e5111a97420ee1
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-10 13:33:00 +0100

    refactored extractResponseData(res: Response => any), added methods till getGenreDetails

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -46,7 +46,40 @@ export class FetchApiDataService {
       .pipe(map(this.extractResponseData), catchError(this.handleError));
   }
 
-  private extractResponseData(res: Response): any {
+  public getMovieDetails(): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http
+      .get(apiUrl + 'movies', {
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+
+  public getDirectorDetails(directorName: string): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http
+      .get(`${apiUrl}directors/${directorName}`, {
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+
+  public getGenreDetails(movieTitle: string): Observable<any> {
+    const token = localStorage.getItem('token');
+    return this.http
+      .get(`${apiUrl}movies/${movieTitle}/genre`, {
+        headers: new HttpHeaders({
+          Authorization: `Bearer ${token}`,
+        }),
+      })
+      .pipe(map(this.extractResponseData), catchError(this.handleError));
+  }
+
+  private extractResponseData(res: any): any {
     const body = res;
     return body || {};
   }
