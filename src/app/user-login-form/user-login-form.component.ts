f636e2d171db69c27c5f028fc4d72af02e71f0f6
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-14 13:34:58 +0100

    added imports and basic component

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
-  styleUrls: ['./user-login-form.component.scss']
+  styleUrls: ['./user-login-form.component.scss'],
 })
 export class UserLoginFormComponent implements OnInit {
+  @Input() userData = { Username: '', Password: '' };
 
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserLoginFormComponent>,
+    public snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {
+    this.fetchApiData.userLogin(this.userData).subscribe((response) => {
+      console.log(response);
+      this.dialogRef.close();
+      this.snackBar.open('login successful', 'OK', {
+        duration: 2000,
+      }),
+        (response) => {
+          console.log(response);
+          this.snackBar.open(response, 'OK', {
+            duration: 2000,
+          });
+        };
+    });
   }
-
 }
