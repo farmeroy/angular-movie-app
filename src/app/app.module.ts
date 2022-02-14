55b2a5c9b6f2fafde93f7cbdd7f477a68bea44a2
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-14 13:21:52 +0100

    created user-login-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,10 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @NgModule({
-  declarations: [AppComponent, UserRegistrationFormComponent],
+  declarations: [AppComponent, UserRegistrationFormComponent, UserLoginFormComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
