4e2ad9ede52da73774ff769eddc83b5d2203565c
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-18 10:56:08 +0100

    Inject mat dialog

--- src/app/genre-dialog/genre-dialog.component.ts
+++ src/app/genre-dialog/genre-dialog.component.ts
@@ -1,17 +1,14 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import  { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-genre-dialog',
   template: 'passed in {{ data.genre }}',
   templateUrl: './genre-dialog.component.html',
-  styleUrls: ['./genre-dialog.component.scss']
+  styleUrls: ['./genre-dialog.component.scss'],
 })
 export class GenreDialogComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { genre: any }) {}
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { genre: any }) { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
