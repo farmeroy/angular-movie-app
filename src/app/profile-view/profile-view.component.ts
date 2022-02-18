d5913dfc5331af005719db037ced07e3530207d4
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-18 14:13:34 +0100

    fetch user details added

--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-profile-view',
   templateUrl: './profile-view.component.html',
-  styleUrls: ['./profile-view.component.scss']
+  styleUrls: ['./profile-view.component.scss'],
 })
 export class ProfileViewComponent implements OnInit {
+  constructor(public fetchApiData: FetchApiDataService) {}
 
-  constructor() { }
+  user: any = {};
 
   ngOnInit(): void {
+    this.getUserDetails();
   }
 
+  getUserDetails(): void {
+    const username = localStorage.getItem('Username');
+    this.fetchApiData.getUserDetails(username).subscribe((response: any) => {
+      this.user = response;
+      console.log(response);
+      return this.user;
+    });
+  }
 }
