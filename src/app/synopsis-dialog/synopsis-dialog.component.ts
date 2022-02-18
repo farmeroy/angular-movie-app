3d01ec20d406470ef7fafa0ea629d9451c180ede
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-18 11:39:31 +0100

    inject data

--- src/app/synopsis-dialog/synopsis-dialog.component.ts
+++ src/app/synopsis-dialog/synopsis-dialog.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-synopsis-dialog',
+  template: 'passed in {{ data.description }}',
   templateUrl: './synopsis-dialog.component.html',
-  styleUrls: ['./synopsis-dialog.component.scss']
+  styleUrls: ['./synopsis-dialog.component.scss'],
 })
 export class SynopsisDialogComponent implements OnInit {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { description: string }) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
