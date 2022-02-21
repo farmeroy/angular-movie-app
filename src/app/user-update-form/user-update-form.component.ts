b2d776e096aba0b06a6ca24e66f1f7b1c69144a0
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-21 14:21:26 +0100

    update user logic

--- src/app/user-update-form/user-update-form.component.ts
+++ src/app/user-update-form/user-update-form.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { FetchApiDataService } from '../fetch-api-data.service';
 
 @Component({
   selector: 'app-user-update-form',
   templateUrl: './user-update-form.component.html',
-  styleUrls: ['./user-update-form.component.scss']
+  styleUrls: ['./user-update-form.component.scss'],
 })
 export class UserUpdateFormComponent implements OnInit {
+  @Input() userData = {
+    Username: '',
+    Password: '',
+    Email: '',
+    Birthday: '',
+  };
 
-  constructor() { }
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserUpdateFormComponent>
+  ) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  updateUserDetails(): void {
+    const username = localStorage.getItem('Username') || '';
+    this.fetchApiData
+      .updateUserDetails(username, this.userData)
+      .subscribe((response) => {
+        console.log(response);
+        this.dialogRef.close();
+      });
   }
 
+  cancel(): void {
+    this.dialogRef.close();
+  }
 }
